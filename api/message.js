4e48aaaaabbea7be359ed90a6dbeacd60f082190
millenxs <milenameedeiros@gmail.com>
2024-10-14 16:47:45 -0300

    first commit

--- api/message.js
+++ api/message.js
@@ -9,7 +9,7 @@ const genAI = new GoogleGenerativeAI(GOOGLE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const gerarResposta = async (pergunta) => {
-  const prompt = `Você é um especialista em Veganismo. Responda à seguinte pergunta de forma clara e concisa: ${pergunta}`;
+  const prompt = `Você é um chatbot inteligente especializado em fitness e nutrição. Seu objetivo é ajudar os usuários a alcançarem suas metas de saúde e bem-estar por meio de orientações personalizadas em exercícios físicos e dietas equilibradas. Você deve ser capaz de: 1. Criar planos de treino adaptados ao nível de condicionamento físico do usuário, considerando suas preferencias e objetivos (como perda de peso, ganho de massa muscular, melhora resistencia, etc.); 2. Fornecer dicas e informações sobre nutrição, incluindo sugestões de refeições saudáveis, controle de porções e macronutrientes; 3. Responder a perguntas comuns sobre exercícios e nutrição, esclarecendo mitos e fornecendo informações baseadas em evidências; 4. Motivas e incentivar os usuários a manterem-se ativos e saudáveis, oferecendo suporte contínuo e dicas de motivação; 5. Adaptar-se ao feedback dos usuários, ajustando planos de treino e recomentações de nutrição conforme necessário. Lembre-se de ser encorajador e acessível, promovendo um estilo de vida saudável e sustentável. Responda de forma clara e concisa: ${pergunta}`;
 
   try {
     const result = await model.generateContent(prompt);
--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,50 +1,52 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 const ChatBot = () => {
   const [messages, setMessages] = useState([
-    { sender: 'bot', text: 'Olá! Eu sou o VeganBot 🌱. Como posso te ajudar hoje? 😊' }
+    {
+      sender: "bot",
+      text: "Olá! Sou seu assistente de dieta saudável. Me diga o que você tem na geladeira e eu vou te ajudar a planejar uma refeição saudável com base nesses ingredientes! Posso sugerir receitas, ajustar porções e oferecer dicas de substituições saudáveis. Se precisar, também posso te ajudar a montar uma lista de compras para completar sua refeição. O que você tem aí hoje? 😊",
+    },
   ]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [isTyping, setIsTyping] = useState(false);
 
   const chatEndRef = useRef(null);
 
   const scrollToBottom = () => {
-	chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
-	scrollToBottom();
+    scrollToBottom();
   }, [messages]);
 
   const cleanBotMessage = (message) => {
-    return message.replace(/[*#]/g, ''); 
+    return message.replace(/[*#]/g, "");
   };
-
 
   const sendMessage = async () => {
     if (input.trim()) {
-      const userMessage = { text: input, sender: 'user' };
+      const userMessage = { text: input, sender: "user" };
       setMessages((prevMessages) => [...prevMessages, userMessage]);
 
-	  setInput(''); 
+      setInput("");
 
-	  setIsTyping(true);
+      setIsTyping(true);
 
       try {
-        const response = await axios.post('/api/message', { message: input });
+        const response = await axios.post("/api/message", { message: input });
 
         let botMessageText = response.data.reply;
 
-		botMessageText = cleanBotMessage(botMessageText);
-		
-		const botMessage = { text: response.data.reply, sender: 'bot' };
+        botMessageText = cleanBotMessage(botMessageText);
+
+        const botMessage = { text: response.data.reply, sender: "bot" };
         setMessages((prevMessages) => [...prevMessages, botMessage]);
-		setIsTyping(false);
+        setIsTyping(false);
       } catch (error) {
-        console.error('Error communicating with the bot:', error);
-		setIsTyping(false);
+        console.error("Error communicating with the bot:", error);
+        setIsTyping(false);
       }
     }
   };
@@ -52,54 +54,60 @@ const ChatBot = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white/10 rounded-3xl shadow-2xl w-full max-w-md h-[500px] flex flex-col overflow-hidden ">
-        <div className="bg-[#95C457] p-4 flex justify-between items-center ">
-          <div className="title text-black">Chatbot de Veganismo</div>
-          <h2 className="text-gray-700 text-sm">@VeganBot 🌱</h2> 
+        <div className="bg-[#aea80c] p-4 flex justify-between items-center ">
+          <div className="title text-white">Personal Virtual</div>
+          <h2 className="text-white text-sm">@seupersonal </h2>
           <figure className="avatar">
-            <img src="/favicon.png" width="50" height="50" className="rounded-full" />
+            <img src="/" width="50" height="50" className="rounded-full" />
           </figure>
         </div>
 
-        
-        <div className="chat-body flex-1 p-4 overflow-y-auto flex flex-col space-y-2 h-full" id="chat">
+        <div
+          className="chat-body flex-1 p-4 overflow-y-auto flex flex-col space-y-2 h-full"
+          id="chat"
+        >
           {messages.map((msg, index) => (
             <div
               key={index}
               className={
-                msg.sender === 'user'
-                  ? 'bg-gray-600 text-white self-end p-2 rounded-lg max-w-xs'
-                  : 'bg-[#3a5321] text-white self-start p-2 rounded-lg max-w-xs'
+                msg.sender === "user"
+                  ? "bg-black text-white self-end p-2 rounded-lg max-w-xs"
+                  : "bg-[#aea80c] text-white self-start p-2 rounded-lg max-w-xs"
               }
             >
               {msg.text}
             </div>
           ))}
 
-		{isTyping && (
-            <div className="self-start bg-[#3a5321] text-white p-2 rounded-lg max-w-xs">
+          {isTyping && (
+            <div className="self-start bg-[#aea80c] text-white p-2 rounded-lg max-w-xs">
               <div className="typing-indicator">
-                Digitando<span className="dots"><span>.</span><span>.</span><span>.</span></span>
+                Digitando
+                <span className="dots">
+                  <span>.</span>
+                  <span>.</span>
+                  <span>.</span>
+                </span>
               </div>
             </div>
-        )}
+          )}
 
-		  <div ref={chatEndRef} />
+          <div ref={chatEndRef} />
         </div>
 
-       
-        <div className="bg-[#95C457]  p-2 flex">
+        <div className="bg-[#aea80c]  p-2 flex">
           <input
-		  type="text"
+            type="text"
             placeholder="Digite sua mensagem..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            onKeyPress={(e) => e.key === 'Enter' && sendMessage()} 
-            className="flex-grow p-2 bg-transparent ring-0 outline-none border border-neutral-500 text-gray-900 placeholder-gray-700 text-sm rounded-lg focus:ring-gray-700 placeholder-opacity-50 focus:border-gray-800 w-full p-2.5 checked:bg-gray-800 rounded-md outline-none resize-none mb-4"
+            onKeyPress={(e) => e.key === "Enter" && sendMessage()}
+            className="flex-grow p-2 bg-transparent ring-0 outline-none border border-neutral-800 text-black placeholder-gray-800 text-sm rounded-lg focus:ring-gray-700 placeholder-opacity-50 focus:border-gray-200 w-full p-2.5 checked:bg-gray-800 rounded-md outline-none resize-none mb-4"
             rows="3"
           ></input>
           <button
             onClick={sendMessage}
-            className="sendMessage ml-2 bg-[#ABD373] hover:bg-[#e7f3d4] font-bold py-2 px-4 rounded-md mb-4"
+            className="sendMessage ml-2 bg-[#f0d96d] hover:bg-[#f8efce] font-bold py-2 px-4 rounded-md mb-4"
           >
             Enviar
           </button>
